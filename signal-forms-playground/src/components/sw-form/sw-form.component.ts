49a4dbb11046a8623b3cd2de3989a7f74db13215
Jeremy Sarasua <jeremy.sarasua@aveva.com>
2026-01-21 15:54:24 -0800

    create signal form example 2

--- signal-forms-playground/src/components/sw-form/sw-form.component.ts
+++ signal-forms-playground/src/components/sw-form/sw-form.component.ts
@@ -1,23 +1,89 @@
 import { Component, signal } from '@angular/core';
-import { QuantityStepperComponent } from '../quantity-stepper/quantity-stepper.component';
-import { form, FormField, min, required } from '@angular/forms/signals';
+import { email, form, FormField, required } from '@angular/forms/signals';
+
+interface InterestData {
+  name: string;
+  email: string;
+  age: number;
+  subscribe: boolean;
+}
 
 @Component({
   selector: 'app-sw-form',
-  imports: [QuantityStepperComponent, FormField],
+  imports: [FormField],
+  styleUrl: 'sw-form.component.scss',
   template: `
-    <label for="qty">Quantity</label>
-    <app-quantity-stepper id="qty" [formField]="cartForm.quantity" />
-    <p>Value: {{ cartForm.quantity().value() }}</p>
-    @if (cartForm.quantity().invalid()) {
-      <div class="error">Quantity must be at least 1</div>
-    }
+    <h2>Star wars character casting</h2>
+    <form (submit)="onSubmit($event)">
+      <div>
+        <label>
+          Name
+          <input type="name" [formField]="interestForm.name" />
+        </label>
+        @if (interestForm.name().invalid() && interestForm.name().touched()) {
+          <div class="error">
+            @for (error of interestForm.name().errors(); track error.kind) {
+              <span>{{ error.message }}</span>
+            }
+          </div>
+        }
+      </div>
+
+      <div>
+        <label>
+          Email
+          <input type="email" [formField]="interestForm.email" />
+        </label>
+        @if (interestForm.email().invalid() && interestForm.email().touched()) {
+          <div class="error">
+            @for (error of interestForm.email().errors(); track error.kind) {
+              <span>{{ error.message }}</span>
+            }
+          </div>
+        }
+      </div>
+
+      <div>
+        <label>
+          Age
+          <input type="number" [formField]="interestForm.age" />
+        </label>
+        @if (interestForm.age().invalid() && interestForm.age().touched()) {
+          @for (error of interestForm.age().errors(); track error.kind) {
+            <div class="error">
+              <span>{{ error.message }}</span>
+            </div>
+          }
+        }
+      </div>
+
+      <div>
+        <label>
+          <input type="checkbox" [formField]="interestForm.subscribe" />
+          Subscribe me to all emails
+        </label>
+      </div>
+
+      <button type="submit" [disabled]="interestForm().invalid()">Submit</button>
+    </form>
   `,
 })
 export class SwFormComponent {
-  model = signal({ quantity: 1 });
-  cartForm = form(this.model, (schema) => {
-    required(schema.quantity);
-    min(schema.quantity, 1);
+  interestModel = signal<InterestData>({
+    name: '',
+    email: '',
+    age: 0,
+    subscribe: false,
   });
+
+  interestForm = form(this.interestModel, (field) => {
+    required(field.email, { message: 'Email is required' });
+    required(field.name, { message: 'Name is required' });
+    email(field.email, { message: 'Must be valid Email' });
+    required(field.age, { message: 'Age is required' });
+  });
+
+  onSubmit(event: Event) {
+    console.log('SUBMITTED!');
+  }
 }
